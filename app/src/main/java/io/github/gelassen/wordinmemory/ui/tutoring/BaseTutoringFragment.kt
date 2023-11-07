936c7c61ec7f370d4d948f00c0f0f8adfa7fa0b5
Dmitry Kazakov <dmitrii.kazakov@gmail.com>
2023-11-07 10:39:57 +0300

    Identify shallow and deep copy behaviour in kotlin

--- app/src/main/java/io/github/gelassen/wordinmemory/ui/tutoring/BaseTutoringFragment.kt
+++ app/src/main/java/io/github/gelassen/wordinmemory/ui/tutoring/BaseTutoringFragment.kt
@@ -7,14 +7,11 @@ import android.view.Menu
 import android.view.MenuInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.lifecycle.viewModelScope
 import io.github.gelassen.wordinmemory.App
 import io.github.gelassen.wordinmemory.R
 import io.github.gelassen.wordinmemory.model.SubjectToStudy
 import io.github.gelassen.wordinmemory.ui.dashboard.DashboardAdapter
 import io.github.gelassen.wordinmemory.ui.dashboard.DashboardFragment
-import kotlinx.coroutines.cancel
-import kotlinx.coroutines.cancelChildren
 
 abstract class BaseTutoringFragment: DashboardFragment() {
 
@@ -34,9 +31,11 @@ abstract class BaseTutoringFragment: DashboardFragment() {
         binding.completeDailyPractice.apply {
             setOnClickListener {
                 Log.d(App.TAG, "Click on complete daily practice")
+                // FIXME despite on extra reference to target list remain data accessible, it should
+                //  be rewritten by using a deep copy instead of shallow copy https://stackoverflow.com/a/75096673/3649629
                 val dataset = (binding.dashboardList.adapter as DashboardAdapter).getDataset()
-                val deepCopyDataset = buildList { addAll(dataset) }
-                onCompleteDailyPractice(deepCopyDataset.toMutableList())
+                val shallowCopyDataset = buildList { addAll(dataset) }
+                onCompleteDailyPractice(shallowCopyDataset.toMutableList())
             }
         }
     }
