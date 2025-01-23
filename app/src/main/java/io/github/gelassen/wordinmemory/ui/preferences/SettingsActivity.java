6abb0df050728c7268a17cbc26e1c811cd7b0c99
dkazakov <dmitriikazakov@gmail.com>
2025-01-23 19:14:55 +0300

    WIP

---
+++ app/src/main/java/io/github/gelassen/wordinmemory/ui/preferences/SettingsActivity.java
@@ -0,0 +1,30 @@
+package io.github.gelassen.wordinmemory.ui.preferences;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.PreferenceFragmentCompat;
+
+import io.github.gelassen.wordinmemory.R;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.settings, new SettingsFragment())
+                    .commit();
+        }
+    }
+
+    public static class SettingsFragment extends PreferenceFragmentCompat {
+        @Override
+        public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+            setPreferencesFromResource(R.xml.preferences, rootKey);
+        }
+    }
+}
+
